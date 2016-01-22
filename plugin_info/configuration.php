31dc6033b1b8e3b443d824efbe8db2278fd51190
bobox59 <masquelierb@gmail.com>
2016-01-22 16:14:23 +0100

    rename of zip file

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -140,7 +140,7 @@ $ArduinoQty = config::byKey('ArduinoQty', 'arduidom', 1);
                 <!-- <a class="btn btn-danger btn-xs" id="bt_FullDebugDisable"><i class='fa fa-exclamation-triangle'></i> Désactiver les Debugs</a> -->
                 <!-- <a class="btn btn-default btn-sm tooltips expertModeVisible" id="bt_ArduinologDaemon" title="{{Log du demon Arduino}}" style="width : 100%;display: inline-block;"><i class="fa fa-file-o"></i> {{Log des démons}}</a> -->
                 <a class="btn btn-danger btn-xs" id="bt_MigrateArduidom"><i class='fa fa-exclamation-triangle'></i> FORCER Migration des données</a>
-                <a href="plugins/arduidom/ressources/Sketch.zip" class="btn btn-info" id="bt_Download"><i class='fa fa-download'></i> Télécharger les Sketchs (USB+Network)</a>
+                <a href="plugins/arduidom/ressources/Archive.zip" class="btn btn-info" id="bt_Download"><i class='fa fa-download'></i> Télécharger les Sketchs (USB & Shield Ethernet)</a>
             </div>
         </div>
         <hr>
